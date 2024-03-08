7770785b408828fe80a895e34c48766fb44d8c89
Pranav <mpranavprem@gmail.com>
2024-03-08 16:40:25 +0530

    updated code

--- src/lib/database/index.ts
+++ src/lib/database/index.ts
@@ -5,6 +5,7 @@ const mongoDbUrl = process.env.MONGODB_URL;
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDb = async () => {
+  console.log("HELLO")
   if (cached.conn) return cached.conn;
 
   if (!mongoDbUrl) throw new Error("MONGODB_URL is missing");
